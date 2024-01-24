bdaa18a968192bccfa77d919195ba58a19645917
João Neves <joaopedro@outlook.com.br>
2024-01-23 21:35:27 -0300

    chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,21 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+	
+
+	private Color color;
+	
+	public ChessPiece(Board board, Color color) {
+		super(board); //o board será criado pelo construtor da super classe (piece)
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	
+}
